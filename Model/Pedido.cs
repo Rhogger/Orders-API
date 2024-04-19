85f9147c6d2541a5d3a1efbf09a93f1f44c02103
iNattan <natanmart09@gmail.com>
2024-04-19 19:45:03 -0300

    classes produto e item

---
+++ Model/Pedido.cs
@@ -0,0 +1,10 @@
+public class Pedido
+{
+    public string CGC_Cliente { get; set; }
+    public string Nome { get; set; }
+    public string Forma_de_Pagamento { get; set; }
+    public string Data_de_Entrega { get; set; }
+    public double Valor_Total { get; set; }
+    public int Quantidade_de_Itens { get; set; }
+    public List<Item> Lista_de_Itens { get; set; }
+}
